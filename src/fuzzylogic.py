ee46627bf5500e33a8fc578fd953bf29dac40e78
durablemicron <fletchy@hotmail.com.au>
2018-10-20 15:00:57 +0800

    moved the components to its own components file

--- src/fuzzylogic.py
+++ src/fuzzylogic.py
@@ -5,196 +5,12 @@ import random
 from controller import Controller, get_output_rate
 from colour import get_colors,Color
 from button import Button, mousebuttondown
+from components import BatchConverter,Pipe, ChemicalSource
 # Defining my colours
 
 
 colours = get_colors()
 
-
-# Chemical class
-class Chemical:
-	def __init__(self, ctype):
-		self.ctype = ctype
-
-	def render(self,x,y):
-		pygame.draw.circle(screen, self.ctype, (x,y), 5, 0)
-# Pipe class - contain the chemicals and transfer to batch
-class Pipe:
-	def __init__(self, x, y, length, width, batch_converter):
-		self.x = x
-		self.y = y
-		self.length = length
-		self.width = width
-		self.contents = [0]*int(max(abs(length/20),abs(width/20)))
-		self.output = batch_converter
-		self.flowing = 1
-		pipes.append(self)
-	def flow(self):
-		if (self.flowing == 0):
-			return
-		index = len(self.contents) - 1
-		if (self.contents[index] != 0):
-			if (self.output != None):
-				self.output.add(self.contents[index])                        
-		while (index > 0):
-			self.contents[index] = self.contents[index-1]
-			index-=1
-		self.contents[0] = 0
-
-	def render(self):
-		pygame.draw.rect(screen, Color.BLACK.value, (self.x, self.y, self.length, self.width), 1)
-		new_x = self.x + 10*int(self.length/abs(self.length))
-		new_y = self.y + 10*int(self.width/abs(self.width))
-		index = 0
-		while (index < len(self.contents)):
-			if (self.contents[index] != 0):
-				self.contents[index].render(new_x, new_y)
-			index+=1
-			if (abs(self.width) < abs(self.length)):
-				new_x += 20*int(self.length/abs(self.length))
-			else:
-				new_y += 20*int(self.width/abs(self.width))
-	def clearPipe(self):
-		index = len(self.contents) - 1                       
-		while (index > -1):
-			self.contents[index] = 0
-			index-=1
-			
-			
-# Batch Converter class, track how many chemicals are inside and generate
-# new chemicals dependant on the prerequisites
-class BatchConverter:
-	def __init__(self, x, y, ctype, creqs, pipes, name, chance):
-		self.x 			= x
-		self.y 			= y
-		self.ctype 		= ctype
-		self.creqs 		= creqs
-		self.pipes 		= pipes
-		self.basechance = 25
-		self.thres 		= 100
-		self.total 		= 0.0
-		self.chance		= chance
-		self.reactRate  = 1.0
-		self.chemicals 	= numpy.zeros(8,int)
-		self.chemifuzzy = [[100,0,0]]*8
-		self.chemSum	= 0	
-		
-		self.name 		= name
-		self.font_size 	= 28
-		self.font 		= pygame.font.SysFont("georgia", self.font_size)
-		self.color 		= Color.BLACK
-		self.font.set_bold(1)
-		
-		
-	def add(self, chemical):
-		self.chemicals[chemical.ctype] += 1
-	def generate(self):
-		if (self.pipes == None):
-			return 0
-		temp = numpy.subtract(self.chemicals,self.creqs)
-		for index in temp:
-			if (index < 0):
-				return 0
-		for pipe in self.pipes:
-			if (pipe.contents[0] == 0):
-				self.total += self.chance
-				if (self.total >= self.thres):
-					self.total %= (self.thres + 1)
-					#Actually outputted a chemical
-					self.chemicals = temp
-					pipe.contents[0] = Chemical(self.ctype)
-					return 1
-		return 0
-	def render(self):
-		pygame.draw.rect(screen, Color.GRAY.value, (self.x, self.y, 60,60),0)
-		liqLevel = 55*(self.chemSum/100)
-		pygame.draw.rect(screen, Color.BLUE.value, (self.x+45, self.y+55-liqLevel, 10, liqLevel),0)
-		self.font_size = 28
-		screen.blit(self.font.render(self.name, True, self.color.value), [self.x+20, self.y+12])
-		screen.blit(self.font.render(str(self.chance), True, self.color.value), [self.x-20, self.y-12])
-		screen.blit(self.font.render(str(numpy.count_nonzero(get_output_rate())), True, self.color.value), [900, 500])
-	def adapt(self):
-		self.chemSum = 0
-		for chem in self.chemicals:
-			self.chemSum += chem
-		self.chance = self.basechance + (min(self.chemSum*self.reactRate,75))
-	def report(self, draw_x):
-		x = draw_x
-		y = 550
-		w = 15
-		h = 5
-		screen.blit(self.font.render(self.name, True, self.color.value), [x+5, y + 20])
-			
-		index = 0
-		while (index < len(self.chemicals)):
-			if (self.creqs[index] == 1):
-				height = min(self.chemicals[index]*h,250)
-				pygame.draw.rect(screen, colours[index].value, (x,y-height,w,height),0)
-				x+=w
-			index+=1
-	def fuzzify(self):
-		index = 0
-		while (index < len(self.chemicals)):
-			#Apply Fuzzy Logic Membership Function
-			chem = self.chemicals[index]
-			if (chem < 10):
-				low = 100 - chem*10
-				med = 100 - low
-				high = 0
-			elif (chem < 20):
-				low = 0
-				med = 100
-				high = 0
-			elif (chem < 30):
-				low = 0
-				med = 300 - chem*10
-				high = 100 - med
-			else:
-				low = 0
-				med = 0
-				high = 100
-			self.chemifuzzy[index] = [low,med,high]
-			index+=1
-			
-
-class ChemicalSource:
-	def __init__(self, x, y, ctype, pipes, chance, name=""):
-		self.font_size 	= 12
-		self.font 		= pygame.font.SysFont("georgia", self.font_size)
-		self.font.set_bold(1)
-		
-		self.name		= name 
-		
-		self.x 			= x
-		self.y 			= y
-		self.ctype		= ctype
-		self.pipes 		= pipes
-		self.thres 		= 100
-		self.total 		= 0
-		self.chance 	= chance
-		self.color 		= Color.BLACK
-		
-	def adapt(self):
-		nb = self.pipes[0].output
-		nbc = nb.chemifuzzy[self.ctype]
-		nnb = nb.pipes[0].output
-		nnbc = nnb.chemifuzzy[nb.ctype]
-		self.chance = (1/100)*(100-nbc[2])*(nbc[0] + 0.5*nbc[1])*(1/100)*(100-nnbc[2])*(nnbc[0]+0.5*nnbc[1])*(1/100)
-	def generate(self):
-		for pipe in self.pipes:
-			if (pipe.contents[0] == 0):
-				self.total += self.chance*random.uniform(0.8, 1.2)
-				if (self.total > self.thres):
-					self.total %= (self.thres + 1)
-					pipe.contents[0] = Chemical(self.ctype)
-					
-	def render(self):
-		screen.blit(self.font.render(self.name + "=" + str(round(self.chance,2)),
-					True, self.color.value), [self.x, self.y - 20])
-		pygame.draw.rect(screen,colours[self.ctype].value, (self.x, self.y, 20, 20),0)
-
-		
-	
 class Logger:
 	def __init__(self,  name, controller):
 		self.controller		= controller
@@ -398,27 +214,31 @@ batchReactors = []
 sources = []
 
 link_0 = Pipe(840,420,160,20,None)
+pipes.append(link_0)
 batchReactors.append(BatchConverter(780,400,4,[0,0,0,0,0,1,1,0],[link_0],"A",70))
 link_1 = Pipe(680,420,100,20,batchReactors[len(batchReactors)-1])
+pipes.append(link_1)
 link_2 = Pipe(800,260,20,140,batchReactors[len(batchReactors)-1])
+pipes.append(link_2)
 
 batchReactors.append(BatchConverter(620,400,5,[1,0,0,0,0,0,0,1],[link_1],"B",70))
-Pipe(420,420,200,20,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(420,420,200,20,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(400,420,0,[pipes[len(pipes)-1]],20, "B1"))
 link_3 = Pipe(640,80,20,320,batchReactors[len(batchReactors)-1])
+pipes.append(link_3)
 
 batchReactors.append(BatchConverter(780,200,6,[1,1,1,0,0,0,0,0],[link_2],"C",70))
-Pipe(800,100,20,100,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(800,100,20,100,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(800,80,0,[pipes[len(pipes)-1]],20,  "C1"))
-Pipe(500,220,280,20,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(500,220,280,20,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(480,220,1,[pipes[len(pipes)-1]],40, "C2"))
-Pipe(940,220,-100,20,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(940,220,-100,20,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(940,220,2,[pipes[len(pipes)-1]],20, "C3"))
 
 batchReactors.append(BatchConverter(620,20,7,[1,1,0,0,0,0,0,0],[link_3],"D",70))
-Pipe(420,40,200,20,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(420,40,200,20,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(400,40,0,[pipes[len(pipes)-1]],20, "D1"))
-Pipe(880,40,-200,20,batchReactors[len(batchReactors)-1])
+pipes.append(Pipe(880,40,-200,20,batchReactors[len(batchReactors)-1]))
 sources.append(ChemicalSource(880,40,1,[pipes[len(pipes)-1]],40, "D2"))
 
 controller = Controller(batchReactors, sources, pipes)
@@ -555,13 +375,13 @@ while (exit_flag == 0):
 	#Draw simulation elements
 	draw_x = 50
 	for bc in batchReactors:
-		bc.render()
-		bc.report(draw_x)
+		bc.render(screen)
+		bc.report(draw_x,screen)
 		draw_x += 80
 	for pipe in pipes:
-		pipe.render()
+		pipe.render(screen)
 	for source in sources:
-		source.render()
+		source.render(screen)
 
 	
 	# Update screen with new contents
